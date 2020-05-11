89be8a0672b2514603f826447efd3baeb0192e90
Sanket Landge <sanketlandge57@gmail.com>
2020-05-11 17:25:47 +0530

    models updated

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
+admin.site.register(UserProfile)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Reply)
 # Register your models here.
